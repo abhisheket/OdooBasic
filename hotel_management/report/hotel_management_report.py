f9196b1c0340abc4b559d1b37e648e14cd71d59a
Abhishek E T <abhisheketkvr@gmail.com>
2021-05-24 08:56:16 +0530

    Remove Guest column in report.

--- hotel_management/report/hotel_management_report.py
+++ hotel_management/report/hotel_management_report.py
@@ -25,7 +25,6 @@ class HotelReport(models.AbstractModel):
         self._cr.execute("""
             CREATE OR REPLACE VIEW hotel_report AS (
             SELECT row_number() OVER (ORDER BY 1) AS id,
-            '""" + str(data['guest_name']) + """' AS guest,
             hotel_report.check_in, hotel_report.check_out, hotel_report.state
             FROM (SELECT hotel_accommodation.check_in AS check_in,
             hotel_accommodation.check_out AS check_out,
@@ -40,7 +39,7 @@ class HotelReport(models.AbstractModel):
             AND hotel_accommodation.guest_id = """ + str(data['guest_id']) + """
             ORDER BY state, id DESC) AS hotel_report)""")
         self._cr.execute("""
-            SELECT id, guest, check_in, check_out, INITCAP(state)
+            SELECT id, check_in, check_out, INITCAP(state)
             FROM hotel_report""")
         docs = self._cr.fetchall()
         return {
@@ -67,8 +66,6 @@ class HotelReportXLSX(models.AbstractModel):
         self._cr.execute("""
             CREATE OR REPLACE VIEW hotel_report AS (
             SELECT row_number() OVER (ORDER BY 1) AS id,
-            '""" + str(self.env['hotel.report.filter'].browse(
-            [docids.id]).guest_id.name) + """' AS guest,
             hotel_report.check_in, hotel_report.check_out, hotel_report.state
             FROM (SELECT hotel_accommodation.check_in AS check_in,
             hotel_accommodation.check_out AS check_out,
@@ -85,7 +82,7 @@ class HotelReportXLSX(models.AbstractModel):
                 [docids.id]).guest_id.id) + """
             ORDER BY state, id DESC) AS hotel_report)""")
         self._cr.execute("""
-            SELECT id, guest, check_in, check_out, INITCAP(state)
+            SELECT id, check_in, check_out, INITCAP(state)
             FROM hotel_report""")
         docs = self._cr.fetchall()
         sheet = workbook.add_worksheet('Hotel Management Report')
@@ -93,37 +90,35 @@ class HotelReportXLSX(models.AbstractModel):
             {'align': 'center', 'bold': True, 'font_size': '20px'})
         table_head = workbook.add_format({'font_size': '12px', 'bold': True})
         table_values = workbook.add_format({'font_size': '10px'})
-        sheet.merge_range('A1:I2', 'Hotel Management Report', head)
-        sheet.merge_range('A3:I3', '')
-        sheet.merge_range('A4:C4', 'Date From', table_head)
-        sheet.merge_range('A5:C5', self.env['hotel.report.filter'].browse(
+        sheet.merge_range('A1:G2', 'Hotel Management Report', head)
+        sheet.merge_range('A3:G3', '')
+        sheet.merge_range('A4:B4', 'Date From', table_head)
+        sheet.merge_range('A5:B5', self.env['hotel.report.filter'].browse(
             [docids.id]).from_date.strftime('%d/%m/%Y'), table_values)
-        sheet.merge_range('D4:F4', 'Date To', table_head)
-        sheet.merge_range('D5:F5', self.env['hotel.report.filter'].browse(
+        sheet.merge_range('C4:D4', 'Date To', table_head)
+        sheet.merge_range('C5:D5', self.env['hotel.report.filter'].browse(
             [docids.id]).to_date.strftime('%d/%m/%Y'), table_values)
-        sheet.merge_range('G4:I4', 'Guest Name', table_head)
-        sheet.merge_range('G5:I5', self.env['hotel.report.filter'].browse(
+        sheet.merge_range('E4:G4', 'Guest Name', table_head)
+        sheet.merge_range('E5:G5', self.env['hotel.report.filter'].browse(
             [docids.id]).guest_id.name, table_values)
-        sheet.merge_range('A6:I6', '')
+        sheet.merge_range('A6:G6', '')
         sheet.write('A7', 'SL.No', table_head)
-        sheet.merge_range('B7:D7', 'Guest', table_head)
-        sheet.merge_range('E7:F7', 'Check-In', table_head)
-        sheet.merge_range('G7:H7', 'Check-Out', table_head)
-        sheet.write('I7', 'State', table_head)
+        sheet.merge_range('B7:C7', 'Check-In', table_head)
+        sheet.merge_range('D7:E7', 'Check-Out', table_head)
+        sheet.merge_range('F7:G7', 'State', table_head)
         for doc in docs:
             line = doc[0] + 6
             sheet.write(line, 0, doc[0], table_values)
-            sheet.merge_range(line, 1, line, 3, doc[1], table_values)
             sheet.merge_range(
-                line, 4, line, 5, doc[2].astimezone(pytz.timezone(
+                line, 1, line, 2, doc[1].astimezone(pytz.timezone(
                     self.env.user.tz)).strftime('%d/%m/%Y %H:%M:%S'),
                 table_values)
-            if doc[3]:
-                sheet.merge_range(line, 6, line, 7, doc[3].astimezone(
+            if doc[2]:
+                sheet.merge_range(line, 3, line, 4, doc[2].astimezone(
                     pytz.timezone(self.env.user.tz)).strftime(
                     '%d/%m/%Y %H:%M:%S'), table_values)
             else:
                 sheet.merge_range(
-                    line, 6, line, 7, '', table_values)
-            sheet.write(line, 8, doc[4], table_values)
+                    line, 3, line, 4, '', table_values)
+            sheet.merge_range(line, 5, line, 6, doc[3], table_values)
         workbook.close()
