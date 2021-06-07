7782d32dea3e1104d74b24ec24e0a6f386b77aa6
Abhishek E T <abhisheketkvr@gmail.com>
2021-06-07 23:12:27 +0530

    Create custom module in Odoo 13. Add brand field in product page. Add brand in orderline.

---
+++ pos_product_brand_13/static/src/js/order_line.js
@@ -0,0 +1,7 @@
+odoo.define('pos_product_brand_13.order_line', function (require) {
+    'use strict';
+
+    var models = require('point_of_sale.models');
+
+    models.load_fields('product.product', 'brand');
+});
