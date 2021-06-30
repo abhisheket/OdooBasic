c165fa21a140e4f9756b5169ab95cd9642daaf1b
Abhishek E T <abhisheketkvr@gmail.com>
2021-06-30 17:56:09 +0530

    Change the payment page redirection to controller.

--- payment_zaakpay/controllers/main.py
+++ payment_zaakpay/controllers/main.py
@@ -1,1 +1,41 @@
 # -*- coding: utf-8 -*-
+
+import logging
+import pprint
+import requests
+import werkzeug
+
+from odoo import http
+from odoo.addons.payment.models.payment_acquirer import ValidationError
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+
+class ZaakpayController(http.Controller):
+    _return_url = '/payment/zaakpay/return'
+
+    @http.route(['/shop/payment/zaakpay'], type='http', auth='none',
+                method='POST', csrf=None, website=True)
+    def zaakpay_checkout(self, **post):
+        print(post)
+        url = 'https://sandbox.zaakpay.com/transactU?v=8' + \
+              '&merchantIdentifier=' + post.get('merchantIdentifier') + \
+              '&orderId=' + post.get('orderId') + '&mode=' +\
+              post.get('mode') + '&checksum=' + post.get('checksum')
+        print(url)
+        # response = requests.post(url, data=post, headers=header)
+        # print("Test : ", response.text)
+
+    @http.route(['/payment/zaakpay/return'], type='http', auth='public',
+                methods=['POST', 'GET'], csrf=False)
+    def zaakpay_return(self, **post):
+        """ Zaakpay return """
+        _logger.info('Beginning Zaakpay form_feedback with post data %s',
+                     pprint.pformat(post))  # debug
+        # try:
+        #     # res = self.zaakpay_validate_data(**post)
+        # except ValidationError:
+        #     _logger.exception('Unable to validate the Paypal payment')
+        # return werkzeug.utils.redirect('/payment/process')
+        print("Hai")
--- payment_zaakpay/models/payment.py
+++ payment_zaakpay/models/payment.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import datetime
+import hmac
+import hashlib
+import logging
+from datetime import datetime
 from werkzeug import urls
 
 from odoo import api, fields, models
-from odoo.addons.payment.models.payment_acquirer import ValidationError
+from odoo.http import request
 
-import logging
 
 _logger = logging.getLogger(__name__)
 
@@ -22,49 +24,65 @@ class AcquirerZaakpay(models.Model):
     zaakpay_secret_key = fields.Char(
         'Secret Key', required_if_provider='zaakpay', groups='base.group_user')
 
+    @api.model
     def _get_zaakpay_urls(self, environment):
+        print("Get URL")
         """ Zaakpay URLs"""
-        print(environment, "URLs")
         if environment == 'prod':
-            return {'zaakpay_form_url': 'https://api.zaakpay.com/transactU?v=8'}
+            return {'zaakpay_form_url': '/shop/payment/zaakpay'}
         else:
-            return {
-                'zaakpay_form_url': 'https://sandbox.zaakpay.com/transactU?v=8'
-            }
+            return {'zaakpay_form_url': '/shop/payment/zaakpay'}
 
-    def paytm_form_generate_values(self, values):
-        console.log("logging", request.httprequest.environ['REMOTE_ADDR'])
-        self.ensure_one()
+    def zaakpay_form_generate_values(self, values):
+        print("loading...............")
+        print(values)
         base_url = self.env['ir.config_parameter'].sudo().get_param(
             'web.base.url')
-        now = datetime.now()
         zaakpay_values = dict(
-            merchantIdentifier = self.zaakpay_merchant_id,
-            orderId = values.get('reference'),
-            returnUrl = urls.url_join(base_url, '/payment/zaakpay/return/'),
-            buyerEmail = values.get('partner_email'),
-            buyerFirstName = values.get('partner_first_name'),
-            buyerLastName = values.get('partner_last_name'),
-            buyerAddress = values.get('partner_address'),
-            buyerCity = values.get('partner_city'),
-            buyerState = values.get('partner_state').name,
-            buyerPincode = values.get('partner_zip'),
-            buyerPhoneNumber = values.get('partner_phone'),
-            txnType = 11,
-            zpPayOption = 1,
-            mode = 1,
-            currency = values.get('currency').name,
-            amount = float(values['amount']) * 100,
-            merchantIpAddress = ,
-            txnDate = datetime. today().strftime('%Y-%m-%d'),
-            purpose = 0 if self.env['product.template'].type == 'service' else 1,
-            productDescription = 'New order',
-            debitorcredit = 'credit',
+            merchantIdentifier=self.zaakpay_merchant_id,
+            orderId=values.get('reference'),
+            returnUrl=urls.url_join(base_url, 'shop/payment/zaakpay/return/'),
+            buyerEmail=values.get('partner_email'),
+            buyerFirstName=values.get('partner_first_name'),
+            buyerLastName=values.get('partner_last_name'),
+            buyerAddress=values.get('partner_address'),
+            buyerCity=values.get('partner_city'),
+            buyerState=values.get('partner_state').name,
+            buyerPincode=values.get('partner_zip'),
+            buyerPhoneNumber=values.get('partner_phone'),
+            txnType=1,
+            zpPayOption=1,
+            mode=0,
+            currency=values.get('currency').name,
+            amount=int(float(values['amount']) * 100),
+            merchantIpAddress=str(request.httprequest.remote_addr),
+            txnDate=datetime.today().strftime('%Y-%m-%d'),
+            purpose=0 if self.env['product.template'].type == 'service' else 1,
+            productDescription='New order',
+            # debitorcredit='credit',
         )
-        zaakpay_values['reqHashKey'] = self.generate_checksum(zaakpay_values, self.zaakpay_secret_key)
+        zaakpay_values['checksum'] = self.generate_checksum(
+            zaakpay_values, self.zaakpay_secret_key)
+        print(zaakpay_values)
         return zaakpay_values
 
     def zaakpay_get_form_action_url(self):
+        print("Action URL")
         self.ensure_one()
-        environment = 'prod' if self.state == 'enabled' else 'test'
-        return self._get_zaakpay_urls(environment)
+        # environment = 'prod' if self.state == 'enabled' else 'test'
+        # return self._get_zaakpay_urls(environment)['zaakpay_form_url']
+        return urls.url_join(self.get_base_url(), '/shop/payment/zaakpay')
+
+    def generate_checksum(self, param_dict, secret_key):
+        checksum_string = ''
+        for key, value in param_dict.items():
+            if value:
+                if checksum_string:
+                    checksum_string += '&'
+                checksum_string += key + "=" + str(value)
+        print('checksum_string', checksum_string)
+        checksum = hmac.new(bytes(secret_key, 'latin-1'),
+                            msg=bytes(checksum_string, 'latin-1'),
+                            digestmod=hashlib.sha256).hexdigest()
+        print('checksum', checksum)
+        return checksum
